2627dc47cad97ffd547dcaa3a02ba3b91daeab89
Oleksandr Semenov <146380500+RaDvyD@users.noreply.github.com>
2024-10-30 01:14:43 +0200

    dsa
    dsa

---
+++ build/MyDynamicLibrary/CMakeFiles/MyDynamicLibrary.dir/MyLibrary.cpp.obj.d
@@ -0,0 +1,3 @@
+MyDynamicLibrary/CMakeFiles/MyDynamicLibrary.dir/MyLibrary.cpp.obj: \
+ D:\VS\.vscode\.vscode\MyProject\MyDynamicLibrary\MyLibrary.cpp \
+ D:\VS\.vscode\.vscode\MyProject\MyDynamicLibrary\MyLibrary.h
---
+++ build/MyDynamicLibrary/CMakeFiles/MyDynamicLibrary.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyDynamicLibrary.dir/MyLibrary.cpp.obj"
+  "CMakeFiles/MyDynamicLibrary.dir/MyLibrary.cpp.obj.d"
+  "libMyDynamicLibrary.dll"
+  "libMyDynamicLibrary.dll.a"
+  "libMyDynamicLibrary.dll.manifest"
+  "libMyDynamicLibrary.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyDynamicLibrary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
